934b20d8d9603ddc0b85e6eab59b03dd5d5bb46f
Adri <adri.nikce30@gmail.com>
2024-09-07 14:19:13 -0300

    update reamde

--- src/number/number.controller.ts
+++ src/number/number.controller.ts
@@ -1,11 +1,10 @@
-import { Controller, Post, Req, Res } from '@nestjs/common';
+import { Controller, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
 @Controller('number')
 export class NumberController {
   @Post()
   getRandomNumber(@Res() res: Response) {
     let n = Math.floor(Math.random() * 101);
-    
     res.status(200).json({ value: n });
   }
 }
--- src/number/timestamp/timestamp.middleware.ts
+++ src/number/timestamp/timestamp.middleware.ts
@@ -5,6 +5,7 @@ export class TimestampMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: () => void) {
     const timestamp = Date.now().valueOf()
     res.locals.timestamp = timestamp
+    console.log(`New request at ${timestamp}`)
     next();
   }
 }
